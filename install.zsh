0cd00d0d1c63a7de57d374eaa9bcf514fd2dba47
Dracape <SomilGoyal2010@outlook.com>
2025-07-19 20:39:31 +0530

    Better installation script

--- install.zsh
+++ install.zsh
@@ -3,13 +3,13 @@
 # Prepare directory
 rm -rf /tmp/graphene/*
 mkdir /tmp/graphene
+cd /tmp/graphene
 
 # Prepare files
-aria2c -d /tmp/graphene https://raw.githubusercontent.com/Dracape/graphene/main/graphite https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/install.sh https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/graphite.xslt
+aria2c https://raw.githubusercontent.com/Dracape/graphene/main/graphite https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/install.sh https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/graphite.xslt
 chmod +x /tmp/graphene/install.sh
 
 # Install
-cd /tmp/graphene
 ./install.sh
 
 # Clean up
