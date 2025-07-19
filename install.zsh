dd13fbb50ab192c98721a11c6a11e955ef9ccb85
Drazape <Drazape@outlook.com>
2025-07-19 20:22:42 +0530

    Drop plugins

--- install.zsh
+++ install.zsh
@@ -1,44 +1,16 @@
 #!/usr/bin/zsh
 
-set -euo pipefail
+# Prepare directory
+rm -rf /tmp/graphene/*
+mkdir /tmp/graphene
 
-if (( EUID != 0 )); then
-  echo "This script must be run as root." >&2
-  exit 1
-fi
+# Prepare files
+aria2c -d /tmp/graphene https://raw.githubusercontent.com/Dracape/graphene/main/graphite https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/install.sh https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/graphite.xslt
+chmod +x /tmp/graphene/install.sh
 
-WORKDIR="/tmp/graphene"
-LAYOUT_DIR="$WORKDIR/layout"
+# Change to the linux directory and run the installation
+cd /tmp/graphene
+./install.sh
 
-echo "[0/5] Cleaning workspace"
-rm -rf "$WORKDIR"
-
-echo "[1/5] Cloning graphene repository"
-git clone --depth=1 https://github.com/DestroyerBDT/graphene.git "$WORKDIR"
-
-# Remove unwanted top-level files
-rm -f "$WORKDIR/README.md" "$WORKDIR/LICENSE" "$WORKDIR/install.zsh"
-
-# Create layout directory and move graphite layout file
-mkdir -p "$LAYOUT_DIR"
-mv "$WORKDIR/graphite" "$LAYOUT_DIR/graphite"
-
-echo "[2/5] Cloning layout install files from graphite-layout"
-git clone --depth=1 https://github.com/xedrac/graphite-layout.git "$WORKDIR/.tmp-layout"
-
-mv "$WORKDIR/.tmp-layout/linux/install.sh" "$LAYOUT_DIR/install.sh"
-mv "$WORKDIR/.tmp-layout/linux/graphite.xslt" "$LAYOUT_DIR/graphite.xslt"
-rm -rf "$WORKDIR/.tmp-layout"
-
-echo "[3/5] Running layout installer"
-chmod +x "$LAYOUT_DIR/install.sh"
-( cd "$LAYOUT_DIR" && ./install.sh )
-
-echo "[4/5] Executing plugin installation script"
-chmod +x "$WORKDIR/plugins/install.zsh"
-"$WORKDIR/plugins/install.zsh"
-
-echo "[5/5] Cleaning up temporary installation files"
-rm -rf "$WORKDIR"
-
-echo "✔ Installation complete."
+# Clean up the cloned repository and downloaded file
+rm -f /tmp/graphene
