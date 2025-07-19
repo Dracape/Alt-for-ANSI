18b1e5b10920562b81f8ed765e700c09387d88c0
Drazape <Drazape@outlook.com>
2025-07-19 20:23:24 +0530

    Update comments

--- install.zsh
+++ install.zsh
@@ -8,9 +8,9 @@ mkdir /tmp/graphene
 aria2c -d /tmp/graphene https://raw.githubusercontent.com/Dracape/graphene/main/graphite https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/install.sh https://raw.githubusercontent.com/xedrac/graphite-layout/main/linux/graphite.xslt
 chmod +x /tmp/graphene/install.sh
 
-# Change to the linux directory and run the installation
+# Install
 cd /tmp/graphene
 ./install.sh
 
-# Clean up the cloned repository and downloaded file
+# Clean up
 rm -f /tmp/graphene
